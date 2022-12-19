cbebd3eb1b23a219b14630d65bc6c1c47acefdf9
Inna Kirichenko <dawavovak@gmail.com>
2022-12-20 00:17:57 +0100

    add 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,32 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+
+const imgMarkup = createGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', imgMarkup);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>`;
+    })
+    .join('');
+}
+
+var slider = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
