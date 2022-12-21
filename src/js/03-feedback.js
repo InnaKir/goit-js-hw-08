b69fd5a1a6a020c6e4060ec96696b0b3f19876de
Inna Kirichenko <dawavovak@gmail.com>
2022-12-21 01:19:31 +0100

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+// import throttle from 'lodashn.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea');
+
+form.addEventListener('submit', onFormSubmit);
+textarea.addEventListener('input', onTextareaInputnnnn);
+// email.addEventListener('input', onEmailInput);
+
+populateText();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function onTextareaInput(evt) {
+  const message = evt.currentTarget.value;
+  localStorage.setItem('feedback-form-state', message);
+  console.log(message);
+}
+
+// function onEmailInput(evt) {
+//   const valueEmail = evt.currentTarget.value;
+//   localStorage.setItem('feedback-form-state', valueEmail);
+//   console.log(valueEmail);
+// }
+
+function populateText() {
+  const savedText = localStorage.getItem('feedback-form-state');
+  if (savedText) {
+    textarea.value = savedText;
+  }
+}
